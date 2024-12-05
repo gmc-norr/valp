3316394c43df5d5e8f28e8d2ff34d7de3297ab02
Niklas Mähler <niklas.mahler@gmail.com>
2024-12-05 09:20:25 +0100

    Add hap.py summary table and styling

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,30 @@
-alert("you're not a dummy!");
+const infoBoxes = d3.selectAll(".comparison-info");
+const tableRows = d3.selectAll("tbody tr");
+
+infoBoxes.on("mouseenter", (evt) => {
+  const compId = evt.target.dataset.id;
+  const tableRows = d3.selectAll(`tr[data-id="${compId}"]`);
+  evt.target.classList.add("bg-blue-200");
+  tableRows.classed("bg-blue-200", true);
+});
+
+infoBoxes.on("mouseleave", (evt) => {
+  const compId = evt.target.dataset.id;
+  const tableRows = d3.selectAll(`tr[data-id="${compId}"]`);
+  evt.target.classList.remove("bg-blue-200");
+  tableRows.classed("bg-blue-200", false);
+});
+
+tableRows.on("mouseenter", (evt) => {
+  const compId = evt.target.dataset.id;
+  evt.target.classList.add("bg-blue-200");
+  d3.select(`.comparison-info[data-id="${compId}"]`)
+    .classed("bg-blue-200", true);
+});
+
+tableRows.on("mouseleave", (evt) => {
+  const compId = evt.target.dataset.id;
+  evt.target.classList.remove("bg-blue-200");
+  d3.select(`.comparison-info[data-id="${compId}"]`)
+    .classed("bg-blue-200", false);
+});
