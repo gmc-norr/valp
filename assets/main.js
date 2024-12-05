6135d7ee9dac4a98fc5edb45aae834a373503514
Niklas Mähler <niklas.mahler@gmail.com>
2024-12-05 09:59:20 +0100

    Better highlighting

--- assets/main.js
+++ assets/main.js
@@ -4,27 +4,23 @@ const tableRows = d3.selectAll("tbody tr");
 infoBoxes.on("mouseenter", (evt) => {
   const compId = evt.target.dataset.id;
   const tableRows = d3.selectAll(`tr[data-id="${compId}"]`);
-  evt.target.classList.add("bg-blue-200");
-  tableRows.classed("bg-blue-200", true);
+  tableRows.classed("highlight", true);
 });
 
 infoBoxes.on("mouseleave", (evt) => {
   const compId = evt.target.dataset.id;
   const tableRows = d3.selectAll(`tr[data-id="${compId}"]`);
-  evt.target.classList.remove("bg-blue-200");
-  tableRows.classed("bg-blue-200", false);
+  tableRows.classed("highlight", false);
 });
 
 tableRows.on("mouseenter", (evt) => {
   const compId = evt.target.dataset.id;
-  evt.target.classList.add("bg-blue-200");
   d3.select(`.comparison-info[data-id="${compId}"]`)
-    .classed("bg-blue-200", true);
+    .classed("highlight", true);
 });
 
 tableRows.on("mouseleave", (evt) => {
   const compId = evt.target.dataset.id;
-  evt.target.classList.remove("bg-blue-200");
   d3.select(`.comparison-info[data-id="${compId}"]`)
-    .classed("bg-blue-200", false);
+    .classed("highlight", false);
 });
